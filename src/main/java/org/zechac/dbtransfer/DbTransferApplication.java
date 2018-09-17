f7c9d664d82a5d6ad0f0adfdd8a47d8305cf8a67
zhaozh <zhaozh@a.com>
2018-09-17 11:02:15 +0800

    init

---
+++ src/main/java/org/zechac/dbtransfer/DbTransferApplication.java
@@ -0,0 +1,12 @@
+package org.zechac.dbtransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbTransferApplication.class, args);
+	}
+}
